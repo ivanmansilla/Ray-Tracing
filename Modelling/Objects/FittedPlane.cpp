b2075534fb0069217f1468d910fe9b4b6309b9b5
jordiBujaldon <jordibujal@gmail.com>
2022-03-13 12:11:00 +0100

    Apartado 4.2-b: rectifiación métodos read y write de FittedPlane

--- Modelling/Objects/FittedPlane.cpp
+++ Modelling/Objects/FittedPlane.cpp
@@ -15,13 +15,11 @@ void FittedPlane::read(const QJsonObject &json) {
         QJsonArray auxVec = json["pmin"].toArray();
         pmin[0] = auxVec[0].toDouble();
         pmin[1] = auxVec[1].toDouble();
-        pmin[2] = auxVec[2].toDouble();
     }
     if (json.contains("pmax") && json["pmax"].isArray()) {
         QJsonArray auxVec = json["pmax"].toArray();
         pmax[0] = auxVec[0].toDouble();
         pmax[1] = auxVec[1].toDouble();
-        pmax[2] = auxVec[2].toDouble();
     }
 }
 
@@ -31,13 +29,11 @@ void FittedPlane::write(QJsonObject &json) const {
     QJsonArray auxArray;
     auxArray.append(pmin[0]);
     auxArray.append(pmin[1]);
-    auxArray.append(pmin[2]);
     json["pmin"] = auxArray;
 
     QJsonArray auxArray2;
     auxArray2.append(pmax[0]);
     auxArray2.append(pmax[1]);
-    auxArray2.append(pmax[2]);
     json["pmax"] = auxArray2;
 }
 
@@ -46,6 +42,6 @@ void FittedPlane::print(int indentation) const {
 
     const QString indent(indentation * 2, ' ');
 
-    QTextStream(stdout) << indent << "pmin:\t" << pmin[0] << ", "<< pmin[1] << ", "<< pmin[2] << "\n";
-    QTextStream(stdout) << indent << "pmax:\t" << pmax[0] << ", "<< pmax[1] << ", "<< pmax[2] << "\n";
+    QTextStream(stdout) << indent << "pmin:\t" << pmin[0] << ", "<< pmin[1] << "\n";
+    QTextStream(stdout) << indent << "pmax:\t" << pmax[0] << ", "<< pmax[1] << "\n";
 }
