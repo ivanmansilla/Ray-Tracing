41ac66aa3304ee259b25cfcb25e6cc685320c9d7
jordiBujaldon <jordibujal@gmail.com>
2022-04-07 15:44:38 +0200

    Codi de transparencies modificat

--- Modelling/Materials/Transparent.cpp
+++ Modelling/Materials/Transparent.cpp
@@ -15,29 +15,37 @@ Transparent::Transparent(const vec3& a, const vec3& d, const vec3& s, const floa
 Transparent::~Transparent() {}
 
 vec3 Transparent::getAttenuation(const Ray &r_in, const HitInfo &rec) const {
-    vec3 i= normalize(r_in.getDirection());
+    vec3 i = normalize(r_in.getDirection());
     vec3 n = normalize(rec.normal);
-    n=dot(i,n)>0?(-n):n;
-    float ni_over_nt=dot(i,n) > 0 ? nut/1.0:1.0/nut;
-    vec3 t= glm::refract(i,n,ni_over_nt);
 
-    if(length(t)<FLT_EPSILON){
+    if (dot(i, n) > 0) {
+        n = -n;
+    }
+
+    float ni_over_nt = 1.0/nut;
+    vec3 t = glm::refract(i, n, ni_over_nt);
+
+    if (length(t) < FLT_EPSILON) {
         return this->Ks;
-    }else{
+    } else {
         return this->Kt;
     }
 }
 
 bool Transparent::getOneScatteredRay(const Ray &r_in, const HitInfo &rec, Ray &r_out) const {
-    vec3 i= normalize(r_in.getDirection());
+    vec3 i = normalize(r_in.getDirection());
     vec3 n = normalize(rec.normal);
-    n=dot(i,n)>0?(-n):n;
-    float ni_over_nt=dot(i,n) > 0 ? nut/1.0:1.0/nut;
-    vec3 t= glm::refract(i,n,ni_over_nt);
 
-    if(length(t)<FLT_EPSILON){
+    if (dot(i, n) > 0) {
+        n = -n;
+    }
+
+    float ni_over_nt = 1.0/nut;
+    vec3 t = glm::refract(i, n, ni_over_nt);
+
+    if (length(t) < FLT_EPSILON) {
         r_out = Ray(rec.p, glm::reflect(i,n));
-    }else{
+    } else {
         r_out = Ray(rec.p, t);
     }
    return true;
